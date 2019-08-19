3f8441c194de61b78ffdff9919e9191bd466d171
206471378 <35678319+ochenian@users.noreply.github.com>
2019-08-18 22:32:56 -0700

    Config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ToddHoppArt - Portfolio Site for Todd Hoppmeyer`,
+        short_name: `ToddHoppArt`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
