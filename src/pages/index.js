dac7e7e8e14779909fd133bbb54dc22679208eee
206471378 <35678319+ochenian@users.noreply.github.com>
2019-08-18 22:32:29 -0700

    Add social icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,27 +8,61 @@ import Logo from "../assets/svg/logo.inline.svg"
 import SiteNavButton from "../assets/svg/icon-site_nav.inline.svg"
 import StoryboardLink from "../assets/svg/link-storyboards.inline.svg"
 import ContactLink from "../assets/svg/link-contact.inline.svg"
+import FacebookIcon from "../assets/svg/icon-fb.inline.svg"
+import InstagramIcon from "../assets/svg/icon-instagram.inline.svg"
+import TwitterIcon from "../assets/svg/icon-twitter.inline.svg"
 
 const IndexPage = () => (
   <Layout>
+
     <SEO title="Home" />
+
     <StyledSiteNavBtn />
-    <StyledLogo />
-    <LinksContainer>
-      <StyledStoryboardLink />
-      <StyledContactLink />
-    </LinksContainer>
+
+    <LogoLinksContainer>
+      <StyledLogo />
+      <LinksContainer>
+        <Link to="/page-2/">
+          <StyledStoryboardLink />
+        </Link>
+        <Link to="/page-2/">
+          <StyledContactLink />
+        </Link>
+      </LinksContainer>
+    </LogoLinksContainer>
+
+    <SocialIconsContainer>
+      <SocialMediaIconLink href="https://www.facebook.com/todd.hoppmeyer">
+        <StyledFacebookIcon />
+      </SocialMediaIconLink>
+
+      <SocialMediaIconLink href="https://www.instagram.com/treztronic">
+        <StyledInstagramIcon />
+      </SocialMediaIconLink>
+
+      <SocialMediaIconLink href="https://twitter.com/treztronic">
+        <StyledTwitterIcon />
+      </SocialMediaIconLink>
+    </SocialIconsContainer>
 
   </Layout>
 )
+
+const LogoLinksContainer = styled.div`
+  width: 90%;
+  height: 100%;
+  max-width: 700px;
+  margin: 0 auto;
+  margin-bottom: 4em;
+`
 
 const LinksContainer = styled.div`
   display: flex;
   flex-direction: column;
 
   position: relative;
-  left: 44%;
-  margin-top: -16%;
+  left: 42%;
+  margin-top: -24%;
 `
 
 const StyledSiteNavBtn = styled(SiteNavButton)`
@@ -42,21 +76,77 @@ const StyledSiteNavBtn = styled(SiteNavButton)`
 `
 
 const StyledLogo = styled(Logo)`
-  width: 70%;
+  width: 90%;
   max-width: 740px;
   height: auto;
-  margin-left: 4em;
 `
 
 const StyledStoryboardLink = styled(StoryboardLink)`
-  width: 40%;
+  width: 50%;
   height: auto;
+
+  &:hover path.text-color {
+    fill: #FF0095;
+  }
 `
 
 const StyledContactLink = styled(ContactLink)`
-    width: 25%;
-    height: auto;
-    margin: 10em 0 0 1em;
+  width: 30%;
+  height: auto;
+
+  margin: 12px 0 0 20%;
+
+  &:hover path.text-color {
+    fill: #05B0F0;
+  }
 `
+
+const SocialIconsContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 40%;
+  margin: 0 auto;
+  max-width: 300px;
+`
+
+const StyledFacebookIcon = styled(FacebookIcon)`
+  width: 50%;
+  height: auto;
+
+  cursor: pointer;
+
+  &:hover path.fill {
+    fill: #3B5998;
+  }
+`
+
+const StyledInstagramIcon = styled(InstagramIcon)`
+  width: 50%;
+  height: auto;
+
+  cursor: pointer;
+`
+
+const StyledTwitterIcon = styled(TwitterIcon)`
+  width: 50%;
+  height: auto;
+
+  cursor: pointer;
+
+  &:hover path.fill {
+    fill: #38A1F3;
+  }
+`
+
+const SocialMediaIconLink = styled.a`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+
+  cursor: pointer;
+`;
 
 export default IndexPage
